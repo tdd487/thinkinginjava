175c318165a940dc0e88215e18668874bf5b83d2
tdd487 <168700784@qq.com>
2017-03-12 16:31:30 +0800

    构建器初始化之初始化顺序

---
+++ src/main/java/com/tdd/part4/initializationorder/Tag.java
@@ -0,0 +1,10 @@
+package com.tdd.part4.initializationorder;
+
+/**
+ * Created by TDD on 2017/3/12.
+ */
+public class Tag {
+    Tag(int marker){
+        System.out.println("Tag("+marker+")");
+    }
+}
